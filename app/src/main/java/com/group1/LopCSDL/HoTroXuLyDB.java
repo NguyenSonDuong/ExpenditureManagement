9d6633df2dcf060b9fd404d0aa9225d088bc0f87
NguyenSonDuong <hunterdemon9x99@gmail.com>
2020-03-06 11:12:04 +0700

    Thêm 1 lớp xử lý chung cho database lớp này sẽ dùng để lưu trữ các hàm để lử lý với database - Sơn Dương Góp ý

---
+++ app/src/main/java/com/group1/LopCSDL/HoTroXuLyDB.java
@@ -0,0 +1,47 @@
+package com.group1.LopCSDL;
+
+import android.content.Context;
+
+public class HoTroXuLyDB {
+    public static void ttNguoiDung(XuLyDatabase xuLyDatabase,Context context,String namedatabase){
+        if(xuLyDatabase == null){
+            xuLyDatabase = new XuLyDatabase(context, namedatabase, null, 1);
+        }
+        xuLyDatabase.khongTraKQ("CREATE TABLE IF NOT EXISTS NguoiDung (nickName VARCHAR(200) PRIMARY KEY," +
+                "Email VARCHAR(200), " +
+                "GioiTinh INTEGER, " +
+                "ngaySinh DATETIME, " +
+                "PassWordl VARCAR(100), " +
+                "Create_time DATETIME)");
+
+        //xuLyDatabase.traVeKQ("INSERT INTO NguoiDung VALUES()");
+        //xuLyDatabase.traVeKQ("SELECT * FROM NguoiDung Where ");
+    }
+
+    public void ttChiTieu(XuLyDatabase xuLyDatabase,Context context,String namedatabase){
+        if(xuLyDatabase == null){
+            xuLyDatabase = new XuLyDatabase(context, namedatabase, null, 1);
+        }
+
+        xuLyDatabase.khongTraKQ("CREATE TABLE IF NOT EXISTS ChiTieu (ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "soTien DOUBLE, " +
+                "loaiGiaoDich VARCHAR(200), " +
+                "ghiChuGiaoDich VARCHAR(150), " +
+                "ngayGiaoDich DATETIME)");
+    }
+
+    public void ttVayTra(XuLyDatabase xuLyDatabase,Context context,String namedatabase){
+        if(xuLyDatabase == null){
+            xuLyDatabase = new XuLyDatabase(context, namedatabase, null, 1);
+        }
+        xuLyDatabase.khongTraKQ("CREATE TABLE IF NOT EXISTS VayTra (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "soTienVay DOUBLE, " +
+                "soTienTra DOUBLE, " +
+                "nguoiGiaoDich VARCHAR(100), " +
+                "loaiGiaoDich VARCHAR(200), " +
+                "ghiChuGiaoDich VARCHAR(100), " +
+                "thoiGianGiaoDich DATETIME, " +
+                "laiSuat FLOAT, " +
+                "trangThai VARCHAR(100))");
+    }
+}
