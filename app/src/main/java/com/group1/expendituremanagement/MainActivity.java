114a43ee30561b9729b2207c77ede7f92e613b2a
ViNgocAnh <vingocanh60@gmail.com>
2020-03-06 21:08:15 +0700

    tạo Lớp thông tin người dùng, thông tin vay trả, thông tin chi tiêu :Ánh khởi tạo dlNguoiDung, dlChiTieu, dlVayTra và tạo các bảng NguoiDung, ChiTieu, VayTra: ánh

--- app/src/main/java/com/group1/expendituremanagement/MainActivity.java
+++ app/src/main/java/com/group1/expendituremanagement/MainActivity.java
@@ -3,7 +3,6 @@ package com.group1.expendituremanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.group1.LopCSDL.HoTroXuLyDataBase;
 import com.group1.LopCSDL.XuLyDatabase;
@@ -16,9 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
-    }
 
+        hoTroXuLyDataBase.ttNguoiDung(null, this, "dlNguoiDung.sqlite");
+
+    }
 
 
 }
