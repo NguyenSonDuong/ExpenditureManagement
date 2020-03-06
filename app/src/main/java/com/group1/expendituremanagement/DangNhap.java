1e772a9da84ce4c9ca97a85955c4ec1d4a204d92
NguyenSonDuong <hunterdemon9x99@gmail.com>
2020-03-06 15:11:05 +0700

    Thêm toast: Sơn Dương

--- app/src/main/java/com/group1/expendituremanagement/DangNhap.java
+++ app/src/main/java/com/group1/expendituremanagement/DangNhap.java
@@ -3,6 +3,7 @@ package com.group1.expendituremanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class DangNhap extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class DangNhap extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_nhap);
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 }
