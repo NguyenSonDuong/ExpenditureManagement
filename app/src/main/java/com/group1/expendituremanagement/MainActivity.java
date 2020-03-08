89b97fab220284ba9dbc1779d469ede5837ba130
NguyenSonDuong <46594077+NguyenSonDuong@users.noreply.github.com>
2020-03-08 22:45:48 +0700

    Revert "Vianh"

--- app/src/main/java/com/group1/expendituremanagement/MainActivity.java
+++ app/src/main/java/com/group1/expendituremanagement/MainActivity.java
@@ -5,21 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import com.group1.LopCSDL.HoTroXuLyDataBase;
-import com.group1.LopCSDL.XuLyDatabase;
-
 public class MainActivity extends AppCompatActivity {
-
-   HoTroXuLyDataBase hoTroXuLyDataBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<
-        hoTroXuLyDataBase.ttNguoiDung(null, this, "dlNguoiDung.sqlite");
-
+        Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show();
     }
-
-
 }
