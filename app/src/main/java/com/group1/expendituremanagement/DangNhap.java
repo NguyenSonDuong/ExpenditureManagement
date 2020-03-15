1cba8101af3dd43394d2ee1e7851b16b1deef2bd
anhanh2412 <anhanh241299@gmail.com>
2020-03-15 15:43:06 +0700

    code phần đăng nhâp lan anh

--- app/src/main/java/com/group1/expendituremanagement/DangNhap.java
+++ app/src/main/java/com/group1/expendituremanagement/DangNhap.java
@@ -2,15 +2,65 @@ package com.group1.expendituremanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class DangNhap extends AppCompatActivity {
+import com.group1.LopCSDL.HoTroXuLyDataBase;
+import com.group1.LopCSDL.XuLyDatabase;
 
+public class DangNhap extends AppCompatActivity {
+    EditText edit_nickname, edit_password;
+    Button bt_dangnhap, bt_quenmatkhau, bt_taotaikhoan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_nhap);
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+        setWidth();
+        setEvenDangNhap();
+        setEvenTaoTaiKhoan();
+    }
+
+    public void setWidth(){
+        edit_nickname = findViewById(R.id.edit_nickname);
+        edit_password = findViewById(R.id.edit_password);
+        bt_dangnhap = findViewById(R.id.bt_dangnhap);
+        bt_quenmatkhau = findViewById(R.id.bt_quenmatkhau);
+        bt_taotaikhoan = findViewById(R.id.bt_taotaikhoan);
+    }
+
+    public void setEvenDangNhap(){
+        bt_dangnhap.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                HoTroXuLyDataBase.ttNguoiDung(null,DangNhap.this,"NguoiDung");
+                String name = edit_nickname.getText().toString();
+                String pass = edit_password.getText().toString();
+                XuLyDatabase xuLyDatabase = new XuLyDatabase(DangNhap.this,"NguoiDung",null, 1);
+                Cursor cursor = xuLyDatabase.traVeKQ("SELECT * FROM NguoiDung WHERE nickName = '"+name+"' AND PassWord = '"+pass+"'");
+                if(cursor == null){
+                    Toast.makeText(DangNhap.this,"Email hoặc mật khẩu không chính xác", Toast.LENGTH_LONG).show();
+                }else {
+                    Intent intent = new Intent(DangNhap.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    public void setEvenTaoTaiKhoan(){
+        bt_taotaikhoan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DangNhap.this, Dang_Ky.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/group1/expendituremanagement/MainActivity.java
+++ app/src/main/java/com/group1/expendituremanagement/MainActivity.java
@@ -2,6 +2,7 @@ package com.group1.expendituremanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -19,13 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-       hoTroXuLyDataBase.ttNguoiDung(null, this, "thongTin.sqlite");
-        HoTroXuLyDataBase.ttVayTra(null,this,"thongTin.sqlite");
-
-
-
-
 
     }
 
