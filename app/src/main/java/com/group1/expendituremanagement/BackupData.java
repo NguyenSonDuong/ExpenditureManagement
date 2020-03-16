9d06961808f67a095589c8d0b02a366f55e1543f
NguyenSonDuong <hunterdemon9x99@gmail.com>
2020-03-17 03:05:57 +0700

    Thêm giao diện backup, Synch, đăng ký online: Sơn Dương

---
+++ app/src/main/java/com/group1/expendituremanagement/BackupData.java
@@ -0,0 +1,14 @@
+package com.group1.expendituremanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BackupData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_backup_data);
+    }
+}
--- app/src/main/java/com/group1/expendituremanagement/Dang_Ky.java
+++ app/src/main/java/com/group1/expendituremanagement/Dang_Ky.java
@@ -28,24 +28,41 @@ public class Dang_Ky extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang__ky);
+        init();
+        setEvent();
+
+    }
+
+    public void init(){
         edtdEmail = (EditText) findViewById(R.id.edtdEmail);
         edtnhapTenTK = (EditText) findViewById(R.id.edtnhapTenTK);
         edtMK = (EditText) findViewById(R.id.edtPass);
         rdgNam = (RadioButton) findViewById(R.id.rdgNam);
+        edtPass = (EditText) findViewById(R.id.edtPass);
         rdgNu = (RadioButton) findViewById(R.id.rdgNu);
         btnDK = (Button) findViewById(R.id.btnDK);
         checkeddk = (CheckBox) findViewById(R.id.checkeddk);
-
-
-
-
+    }
+    public void setEvent(){
         btnDK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String nickname = edtnhapTenTK.getText().toString();
-                String password  = edtMK.getText().toString();
+                String password  = edtPass.getText().toString();
                 String email = edtdEmail.getText().toString();
-
+                String rePass = edtMK.getText().toString();
+                if(!checkeddk.isChecked()){
+                    Toast.makeText(Dang_Ky.this, "Vui lòng đồng ý với điều khoản để tiếp tục đăng ký", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!password.equals(rePass)){
+                    Toast.makeText(Dang_Ky.this, "Mật khẩu không trùng khớp nhau", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(nickname.isEmpty() || password.isEmpty() || email.isEmpty()){
+                    Toast.makeText(Dang_Ky.this, "Vui lòng nhập đủ thông tin", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 int sex = 1;
                 if(rdgNam.isChecked()){
                     sex=2;
@@ -53,18 +70,48 @@ public class Dang_Ky extends AppCompatActivity {
                 XuLyDatabase xuLyDatabase = new XuLyDatabase(Dang_Ky.this, KeyDatabase.DATABASENAME,null,1);
                 boolean dangky = HoTroXuLyDataBase.registerUserSQLLite(xuLyDatabase,nickname,password,email,sex,"");
                 if(dangky){
-                    Toast.makeText(Dang_Ky.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
-                    finish();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(Dang_Ky.this);
+                    builder.setTitle("Thông báo đăng ký online");
+                    builder.setMessage("Bạn đã đăng ký thành công. Bạn có muốn chúng tôi đăng ký tài khoản online cho bạn để bạn có thể đồng bộ dữ liệu lên máy chủ của chứng tôi không");
+                    builder.setNegativeButton("Đồng ý", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            XuLyServer.RegisterReponsiveClass online = registerOnline();
+                            if(online != null){
+                                if(online.table_chi_tieu.equals("Thất bại") || online.table_vay.equals("Thất bại")){
+                                    Toast.makeText(Dang_Ky.this, "Đăng ký online không thành công", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else {
+                                Toast.makeText(Dang_Ky.this, "Đăng ký online không thành công", Toast.LENGTH_SHORT).show();
+                            }
+                            Dang_Ky.this.finish();
+                        }
+                    });
+                    builder.setPositiveButton("Không đồng ý", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Dang_Ky.this.finish();
+                        }
+                    });
+                    builder.show();
                 }else {
                     Toast.makeText(Dang_Ky.this, "Đăng ký thất bại", Toast.LENGTH_SHORT).show();
                 }
-                //XuLyServer.RegisterReponsiveClass registerReponsiveClass = XuLyServer.getReponsiveRegister(Dang_Ky.this,edtnhapTenTK.getText().toString(),edtMK.getText().toString(),edtdEmail.getText().toString(),a);
-//                if(registerReponsiveClass == null){
-//                    //Toast.makeText(getApplicationContext(), "Lỗi", Toast.LENGTH_SHORT).show();
-//                }else {
-//                    //Toast.makeText(getApplicationContext(), "Nickname: "+registerReponsiveClass.nickname, Toast.LENGTH_SHORT).show();
-//                }
             }
         });
+    }
+
+    public XuLyServer.RegisterReponsiveClass registerOnline(){
+        int sex = 1;
+        if(rdgNam.isChecked()){
+            sex=2;
+        }
+        XuLyServer.RegisterReponsiveClass registerReponsiveClass = XuLyServer.getReponsiveRegister(Dang_Ky.this,edtnhapTenTK.getText().toString(),edtMK.getText().toString(),edtdEmail.getText().toString(),sex);
+        if(registerReponsiveClass == null){
+            return null;
+        }else {
+            return registerReponsiveClass;
+        }
     }
 }
