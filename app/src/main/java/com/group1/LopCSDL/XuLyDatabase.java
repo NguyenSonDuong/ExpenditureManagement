c029513b0677c622c5451f9ec09e6b5a2fa2ebf8
NguyenSonDuong <hunterdemon9x99@gmail.com>
2020-03-16 14:44:00 +0700

    Cập nhật và sữa lỗi code chung tối ưu code clean code cho đẹp hơn nè: Sơn Dương

--- app/src/main/java/com/group1/LopCSDL/XuLyDatabase.java
+++ app/src/main/java/com/group1/LopCSDL/XuLyDatabase.java
@@ -9,8 +9,10 @@ import androidx.annotation.Nullable;
 
 public class XuLyDatabase extends SQLiteOpenHelper {
 
+    Context context;
     public XuLyDatabase(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
+        this.context = context;
     }
 
     public void khongTraKQ(String kq){
@@ -25,7 +27,29 @@ public class XuLyDatabase extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        khongTraKQ("CREATE TABLE IF NOT EXISTS "+KeyDatabase.TABLENAME_NGUOIDUNG+" (nickName VARCHAR(200) PRIMARY KEY," +
+                "Email VARCHAR(200), " +
+                "GioiTinh INTEGER, " +
+                "ngaySinh DATETIME, " +
+                "PassWord VARCAR(100), " +
+                "Create_time DATETIME)");
+        khongTraKQ("CREATE TABLE IF NOT EXISTS "+KeyDatabase.TABLENAME_CHITIEU+" (ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "soTien DOUBLE, " +
+                "loaiGiaoDich VARCHAR(200), " +
+                "ghiChuGiaoDich VARCHAR(150), " +
+                "thoiGianGiaoDich DATETIME, " +
+                "diaDiem VARCHAR(200), " +
+                "soLuong INTERGER)");
+        khongTraKQ("CREATE TABLE IF NOT EXISTS "+KeyDatabase.TABLENAME_VAY+" (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "soTienVay DOUBLE, " +
+                "soTienTra DOUBLE, " +
+                "hantra DATETIME"+
+                "nguoiGiaoDich VARCHAR(100), " +
+                "loaiGiaoDich VARCHAR(200), " +
+                "ghiChuGiaoDich VARCHAR(100), " +
+                "thoiGianGiaoDich DATETIME, " +
+                "laiSuat FLOAT, " +
+                "trangThai VARCHAR(100))");
     }
 
     @Override
--- app/src/main/java/com/group1/expendituremanagement/DangNhap.java
+++ app/src/main/java/com/group1/expendituremanagement/DangNhap.java
@@ -10,12 +10,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.group1.LopCSDL.HoTroXuLyDataBase;
+import com.group1.LopCSDL.KeyDatabase;
 import com.group1.LopCSDL.XuLyDatabase;
 
 public class DangNhap extends AppCompatActivity {
     EditText edit_nickname, edit_password;
     Button bt_dangnhap, bt_quenmatkhau, bt_taotaikhoan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,12 +40,11 @@ public class DangNhap extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                HoTroXuLyDataBase.ttNguoiDung(null,DangNhap.this,"NguoiDung");
                 String name = edit_nickname.getText().toString();
                 String pass = edit_password.getText().toString();
-                XuLyDatabase xuLyDatabase = new XuLyDatabase(DangNhap.this,"NguoiDung",null, 1);
-                Cursor cursor = xuLyDatabase.traVeKQ("SELECT * FROM NguoiDung WHERE nickName = '"+name+"' AND PassWord = '"+pass+"'");
-                if(cursor == null){
+                XuLyDatabase xuLyDatabase = new XuLyDatabase(DangNhap.this, KeyDatabase.DATABASENAME,null, 1);
+                Cursor cursor = xuLyDatabase.traVeKQ("SELECT * FROM "+KeyDatabase.TABLENAME_NGUOIDUNG+" WHERE nickName = '"+name+"' AND PassWord = '"+pass+"'");
+                if(cursor.getCount() != 1){
                     Toast.makeText(DangNhap.this,"Email hoặc mật khẩu không chính xác", Toast.LENGTH_LONG).show();
                 }else {
                     Intent intent = new Intent(DangNhap.this, MainActivity.class);
